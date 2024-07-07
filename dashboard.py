bbf5c04f7f3ce7affe53e863a98c8f20221cbd3c
GiovanniStephens <stephens.giovanni@gmail.com>
2024-07-07 13:20:17 +1200

    Plotly Dash dashboard for showing the data.

---
+++ dashboard.py
@@ -0,0 +1,179 @@
+import dash
+from dash import dcc
+from dash import html
+import pandas as pd
+import plotly.express as px
+from dash.dependencies import Input, Output, State
+from dotenv import load_dotenv
+import utils
+
+
+try:
+    load_dotenv()
+    load_dotenv('config.env')
+except FileNotFoundError:
+    print('No config file found.')
+
+
+def fetch_data(supabase):
+    data = supabase.table("Listings").select("*").execute()
+    data_df = pd.DataFrame(data.data)
+    return data_df
+
+
+app = dash.Dash(__name__)
+data = fetch_data(utils.connect_to_supabase())
+data['Price'] = data['Price'] / 1000000
+
+# Generate options for dropdowns
+regions = [{'label': region, 'value': region} for region in data['Region'].unique()]
+regions.insert(0, {'label': 'All Regions', 'value': 'All Regions'})
+districts = [{'label': district, 'value': district} for district in data['District'].unique()]
+districts.insert(0, {'label': 'All Districts', 'value': 'All Districts'})
+suburbs = [{'label': suburb, 'value': suburb} for suburb in data['Suburb'].unique()]
+suburbs.insert(0, {'label': 'All Suburbs', 'value': 'All Suburbs'})
+property_types = [{'label': ptype, 'value': ptype} for ptype in data['PropertyType'].unique()]
+property_types.insert(0, {'label': 'All Property Types', 'value': 'All Property Types'})
+
+# Define the layout of the app
+app.layout = html.Div([
+    html.Div([
+        html.H2('Filters', style={'color': 'white'}),
+        html.Label('Region', style={'color': 'white'}),
+        dcc.Dropdown(id='region-dropdown', options=regions, value='All Regions', style={'marginBottom': '20px'}),
+        html.Label('District', style={'color': 'white'}),
+        dcc.Dropdown(id='district-dropdown', options=districts, value='All Districts', style={'marginBottom': '20px'}),
+        html.Label('Suburb', style={'color': 'white'}),
+        dcc.Dropdown(id='suburb-dropdown', options=suburbs, value='All Suburbs', style={'marginBottom': '20px'}),
+        html.Label('Price Range', style={'color': 'white'}),
+        dcc.RangeSlider(id='price-slider', min=data['Price'].min(), max=data['Price'].max(), step=0.5,
+                        marks={price: f"${price:.1f}m" for price in range(int(data['Price'].min()),
+                                                                          int(data['Price'].max()) + 1,
+                                                                          1 if data['Price'].max() < 5
+                                                                          else 5)},
+                        value=[data['Price'].min(), data['Price'].max()]),
+        html.Label('Bedrooms:', style={'color': 'white'}),
+        dcc.RangeSlider(id='bedrooms-slider', min=0, max=data['Bedrooms'].max(),
+                        step=1, marks={i: str(i) for i in range(int(data['Bedrooms'].max()) + 1)},
+                        value=[0, data['Bedrooms'].max()]),
+        html.Br(),
+        html.Label('Bathrooms:', style={'color': 'white'}),
+        dcc.RangeSlider(id='bathrooms-slider', min=0, max=data['Bathrooms'].max(),
+                        step=1, marks={i: str(i) for i in range(int(data['Bathrooms'].max()) + 1)},
+                        value=[0, data['Bathrooms'].max()]),
+        html.Br(),
+        html.Label('Property Type', style={'color': 'white'}),
+        dcc.Dropdown(id='property-type-dropdown', options=property_types, value='All Property Types',
+                     style={'marginBottom': '20px'}),
+        html.Button(id='filter-button', n_clicks=0, children='Apply Filters'),
+        html.Div(id='stats-div', style={'color': 'white', 'marginTop': '20px'})
+    ],  style={'width': '20%', 'float': 'left', 'backgroundColor': 'navy', 'padding': '20px',
+               'position': 'fixed', 'height': '100vh', 'overflow': 'auto'}),
+
+    html.Div([
+        dcc.Graph(id='price-distribution', style={'height': '800px'}),
+        dcc.Graph(id='listings-on-map', style={'height': '800px'}),
+        dcc.Graph(id='listings-by-property-type', style={'height': '800px'}),
+        dcc.Graph(id='price-vs-land-area', style={'height': '800px'})
+    ], style={'display': 'inline-block', 'width': '75%', 'padding': '20px', 'float': 'right'})
+])
+
+
+@app.callback(
+    [Output('district-dropdown', 'options'),
+     Output('suburb-dropdown', 'options'),
+     Output('property-type-dropdown', 'options')],
+    [Input('region-dropdown', 'value'),
+     Input('district-dropdown', 'value'),
+     Input('suburb-dropdown', 'value')]
+)
+def update_dropdowns(selected_region, selected_district, selected_suburb):
+    filtered_data = data
+
+    if selected_region != 'All Regions':
+        filtered_data = filtered_data[filtered_data['Region'] == selected_region]
+
+    if selected_district != 'All Districts':
+        filtered_data = filtered_data[filtered_data['District'] == selected_district]
+
+    if selected_suburb != 'All Suburbs':
+        filtered_data = filtered_data[filtered_data['Suburb'] == selected_suburb]
+    districts = [{'label': 'All Districts', 'value': 'All Districts'}]\
+        + [{'label': district, 'value': district} for district in filtered_data['District'].unique()]
+    suburbs = [{'label': 'All Suburbs', 'value': 'All Suburbs'}]\
+        + [{'label': suburb, 'value': suburb} for suburb in filtered_data['Suburb'].unique()]
+    property_types = [{'label': 'All Property Types', 'value': 'All Property Types'}]\
+        + [{'label': ptype, 'value': ptype} for ptype in filtered_data['PropertyType'].unique()]
+    return districts, suburbs, property_types
+
+
+# Callback to update the graphs based on filters
+@app.callback(
+    [Output('price-distribution', 'figure'),
+     Output('listings-on-map', 'figure'),
+     Output('listings-by-property-type', 'figure'),
+     Output('price-vs-land-area', 'figure'),
+     Output('stats-div', 'children')],
+    [Input('filter-button', 'n_clicks')],
+    [State('region-dropdown', 'value'),
+     State('district-dropdown', 'value'),
+     State('suburb-dropdown', 'value'),
+     State('price-slider', 'value'),
+     State('bedrooms-slider', 'value'),
+     State('bathrooms-slider', 'value'),
+     State('property-type-dropdown', 'value')]
+)
+def update_graphs(n_clicks, region, district, suburb, price_range, bedroom_range, bathroom_range, property_type):
+    filtered_data = data
+    filtered_data_with_nulls = data
+    if region != 'All Regions':
+        filtered_data = filtered_data[filtered_data['Region'] == region]
+        filtered_data_with_nulls = filtered_data_with_nulls[(filtered_data_with_nulls['Region'] == region) |
+                                                            (filtered_data_with_nulls['Region'].isnull())]
+    if district != 'All Districts':
+        filtered_data = filtered_data[filtered_data['District'] == district]
+        filtered_data_with_nulls = filtered_data_with_nulls[(filtered_data_with_nulls['District'] == district) |
+                                                            (filtered_data_with_nulls['District'].isnull())]
+    if suburb != 'All Suburbs':
+        filtered_data = filtered_data[filtered_data['Suburb'] == suburb]
+        filtered_data_with_nulls = filtered_data_with_nulls[(filtered_data_with_nulls['Suburb'] == suburb) |
+                                                            (filtered_data_with_nulls['Suburb'].isnull())]
+    filtered_data = filtered_data[(filtered_data['Price'] >= price_range[0])
+                                  & (filtered_data['Price'] <= price_range[1])]
+    filtered_data_with_nulls = filtered_data_with_nulls[((filtered_data_with_nulls['Price'] >= price_range[0])
+                                                        & (filtered_data_with_nulls['Price'] <= price_range[1])) |
+                                                        (filtered_data_with_nulls['Price'].isnull())]
+    filtered_data = filtered_data[(filtered_data['Bedrooms'] >= bedroom_range[0])
+                                  & (filtered_data['Bedrooms'] <= bedroom_range[1])]
+    filtered_data_with_nulls = filtered_data_with_nulls[((filtered_data_with_nulls['Bedrooms'] >= bedroom_range[0])
+                                                        & (filtered_data_with_nulls['Bedrooms'] <= bedroom_range[1])) |
+                                                        (filtered_data_with_nulls['Bedrooms'].isnull())]
+    filtered_data = filtered_data[(filtered_data['Bathrooms'] >= bathroom_range[0])
+                                  & (filtered_data['Bathrooms'] <= bathroom_range[1])]
+    filtered_data_with_nulls = filtered_data_with_nulls[((filtered_data_with_nulls['Bathrooms'] >= bathroom_range[0])
+                                                        & (filtered_data_with_nulls['Bathrooms'] <= bathroom_range[1]))
+                                                        | (filtered_data_with_nulls['Bathrooms'].isnull())]
+    if property_type != 'All Property Types':
+        filtered_data = filtered_data[filtered_data['PropertyType'] == property_type]
+        filtered_data_with_nulls = filtered_data_with_nulls[(filtered_data_with_nulls['PropertyType'] == property_type)
+                                                            | (filtered_data_with_nulls['PropertyType'].isnull())]
+
+    fig_histogram = px.histogram(filtered_data, x='Price', title='Price Distribution')
+    fig_map = px.scatter_mapbox(filtered_data, lat='Latitude', lon='Longitude', size='Price', zoom=10, height=300)
+    fig_map.update_layout(mapbox_style='open-street-map', title='Listings on Map', height=800)
+    fig_bar = px.bar(filtered_data['PropertyType'].value_counts().reset_index(),
+                     x='index', y='PropertyType', title='Number of Listings by Property Type')
+    fig_scatter = px.scatter(filtered_data, x='LandArea', y='Price', title='Price vs. Land Area')
+    # Get filtered data but with the null prices
+
+    count = len(filtered_data_with_nulls)
+    median_price = filtered_data['Price'].median() if count > 0 else 0
+    stats_text = [
+        html.Div(f"Count: {count}", style={'marginBottom': '10px'}),
+        html.Div(f"Median Price: ${median_price:.2f}m")
+    ]
+    return fig_histogram, fig_map, fig_bar, fig_scatter, stats_text
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
